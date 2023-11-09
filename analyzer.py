122f432a544e56ae9b8798c46d23b827d1445eac
Aroooba <arooba.shahoor@gmail.com>
2023-11-09 11:37:19 +0900

    Update analyzer to add reference code

--- analyzer.py
+++ analyzer.py
@@ -5,7 +5,36 @@ from typing import Any, Dict, List, Set, Optional, Union, Tuple, Type
 
 
 def getClassNames(path: str) -> List[str]:
-    return list()
+    with open(path) as f:
+        code = f.read()
+
+    my_ast = parse(code)
+
+    clazznames = []
+
+    for path, node in my_ast.filter(javalang.tree.ClassDeclaration):
+        clazznames.append(node.name)
+
+    return clazznames
 
 def getMethodNames(path: str) -> Dict[str, List[str]]:
-    return dict()
+    with open(path) as f:
+        code = f.read()
+
+    my_ast = parse(code)
+
+    methodmap = {}
+
+    for path, node in my_ast.filter(javalang.tree.ClassDeclaration):
+        methodlist = node.methods
+        methodnames = []
+        for amethod in methodlist:
+            methodnames.append(amethod.name)
+
+        methodmap[node.name] = methodnames
+
+    return methodmap
+
+if __name__ == '__main__':
+    amap = getMethodNames("in.java")
+    print(amap)
